aa7bdf4aa15a5f1394ee70576984cecbc4e6bcdb
PonchonB <bastien.ponchon@gmail.com>
2018-07-31 18:32:52 +0200

    KL_sum testing

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -3,14 +3,14 @@ from testsShallowAE import testShallowAE
 from testKLdiv import test_KL_div
 from testShallowAEwithAMD import testShallowAEwithAMD
 
-dims = [1, 5, 10, 20, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 784]
+#dims = [1, 5, 10, 20, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 784]
 
 #testShallowAE(latent_dimensions=dims, svm=True)
 
-testShallowAEwithAMD(latent_dimensions=dims, svm=True)
+#testShallowAEwithAMD(latent_dimensions=dims, svm=True)
 
-#sparsity_weights = [0.01, 0.1, 0.5, 1, 10]
-#sparsity_objectives = [0.01, 0.05, 0.1, 0.2]
+sparsity_weights = [0.01, 0.1, 0.5, 1, 10]
+sparsity_objectives = [0.01, 0.05, 0.1, 0.2]
 
-#test_KL_div(sparsity_weights=sparsity_weights, sparsity_objectives=sparsity_objectives, svm=True)
+test_KL_div(sparsity_weights=sparsity_weights, sparsity_objectives=sparsity_objectives, svm=True)
 
--- SourceCode/run_python_src.sh
+++ SourceCode/run_python_src.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=2
-nohup python main.py 2>../Logs/18_07_12_log_testShallowAEwithAMD >../ShallowAE/WithAMD/Simple/TestOutputs/18_07_12_testShallowAEwithAMD &
+source StageENV/bin/activate
+nohup python main.py 2>../Logs/18_07_31_log_testKLsum >../ShallowAE/Sparse/KL_div_sum/TestOutputs/18_07_12_testShallowAEwithAMD &
+deactivate
