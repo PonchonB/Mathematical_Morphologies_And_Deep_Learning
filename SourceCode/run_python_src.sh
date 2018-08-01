b2d0e5396883d70f2454738420b330a0e5cb07e2
PonchonB <bastien.ponchon@gmail.com>
2018-08-01 10:32:25 +0200

    Editing the bash script to activate and deactivate the environment

--- SourceCode/run_python_src.sh
+++ SourceCode/run_python_src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=2
-source StageENV/bin/activate
+source /home/ponchon/StageENV/bin/activate
 nohup python main.py 2>../Logs/18_07_31_log_testKLsum >../ShallowAE/Sparse/KL_div_sum/TestOutputs/18_07_12_testShallowAEwithAMD &
 deactivate
