8ffb1b5e06eafa28cbe6ff970c3af324dca93b0e
PonchonB <bastien.ponchon@gmail.com>
2018-08-02 18:26:59 +0200

    Modification main

--- SourceCode/run_python_src.sh
+++ SourceCode/run_python_src.sh
@@ -2,5 +2,5 @@
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=2
 source /home/ponchon/StageENV/bin/activate
-nohup python main.py 2>../Logs/18_08_02_log_100_with_and_without_AMD >../ShallowAE/SeveralChannels/18_08_02_dim100_with_and_without_AMD &
+nohup python main.py 2>../Logs/18_08_02_log_100_with_and_without_NonNegConstraint >../ShallowAE/Simple/18_08_02_dim100_with_and_without_NonNegConstraint &
 deactivate
