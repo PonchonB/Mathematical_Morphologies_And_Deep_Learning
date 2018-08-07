440220e01ccad113027eb604c855bf917b87445b
PonchonB <bastien.ponchon@gmail.com>
2018-08-07 22:29:00 +0200

    merge

--- SourceCode/run_python_src.sh
+++ SourceCode/run_python_src.sh
@@ -2,5 +2,5 @@
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=2
 source /home/ponchon/StageENV/bin/activate
-nohup python main.py 2>../Logs/18_08_06_log_100_KLsum_NonNegConstraint_withAMD >../ShallowAE/SeveralChannels/WithAMD/Sparse_NonNeg/KLdivSum_NonNegConstraint/TestOutputs/18_08_06_dim100_KLdivSum_NonNegConstraint_withAMD &
+nohup python main.py 2>../Logs/18_08_07_log_100_AMD_Simple_noOriginal_and_normalized >../ShallowAE/SeveralChannels/WithAMD_NoOriginals/Simple/TestOutputs/18_08_07_dim100_AMD_noOriginals_Normalized_channels &
 deactivate
