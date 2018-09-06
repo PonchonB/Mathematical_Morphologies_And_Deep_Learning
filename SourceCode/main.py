1ba1fb0c1481bb76c9d9a0fc78ada4be8ec9f54d
PonchonB <bastien.ponchon@gmail.com>
2018-09-06 14:28:38 +0200

    AsymAE with AMD

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -9,6 +9,7 @@ import keras
 from AsymAE_infoGAN.testAsymAE import testDims_AsymAE
 from AsymAE_infoGAN.AsymAE_testKLdiv import test_KL_div_Asym_AE
 from AsymAE_infoGAN.nonNegSparseAsymAEinfoGAN import Sparse_NonNeg_AsymAEinfoGAN_KLsum_NonNegConstraint
+from AsymAE_infoGAN.AsymAE_infoGAN import AsymAEinfoGAN
 
 print("Keras version: ", keras.__version__)
 #dims = [1, 5, 10, 20, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 784]
@@ -114,10 +115,16 @@ sparsity_objectives = [0.01, 0.05, 0.1, 0.2]
 ### testing simple AE with AEinfoGAN as encoder
 #testDims_AsymAE(svm=True)
 
-###18_08_04
-### testing simple AE with AEinfoGAN and KLdivSum/NonNegConstraint
+###18_09_04
+### testing AE with AEinfoGAN and KLdivSum/NonNegConstraint
 ### Note that the repository hierarchy has been changed (putting all Models/TestOutputs in a Results directory)
-test_KL_div_Asym_AE(AsymAE_class=Sparse_NonNeg_AsymAEinfoGAN_KLsum_NonNegConstraint, sparsity_weights = sparsity_weights, sparsity_objectives = sparsity_objectives, latent_dimension=100, nb_epochs=500, 
-                nb_input_channels=1, one_channel_output=True, add_original_images=True,
-                AMD=False, AMD_step=1, AMD_init_step=1, svm=False, 
-                path_to_dir = "../Results/AsymAE_infoGAN/")
+#test_KL_div_Asym_AE(AsymAE_class=Sparse_NonNeg_AsymAEinfoGAN_KLsum_NonNegConstraint, sparsity_weights = sparsity_weights, sparsity_objectives = sparsity_objectives, latent_dimension=100, nb_epochs=500, 
+#                nb_input_channels=1, one_channel_output=True, add_original_images=True,
+#                AMD=False, AMD_step=1, AMD_init_step=1, svm=False, 
+#                path_to_dir = "../Results/AsymAE_infoGAN/")
+
+###18_09_06
+### testing AE with AEinfoGAN, Simple and AMD
+testDims_AsymAE(AsymAE_class=AsymAEinfoGAN, latent_dimensions=[100], nb_epochs=500, nb_input_channels=6, one_channel_output=True,
+            AMD=True, AMD_step=1, AMD_init_step=1, add_original_images=False,
+            svm=True, path_to_dir = "../AsymAE_infoGAN/")
