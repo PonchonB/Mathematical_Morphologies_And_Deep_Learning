59cd2ce2e242fdb42649a95ec5b0e9ed5f3b18f6
PonchonB <bastien.ponchon@gmail.com>
2018-09-11 13:23:33 +0200

    Test openings and ShallowAE nonNegSparse with originals

--- SourceCode/run_python_src.sh
+++ SourceCode/run_python_src.sh
@@ -2,5 +2,6 @@
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=1
 source /home/ponchon/StageENV/bin/activate
-nohup python main.py 2>../Logs/18_09_08_log_100_ShallowAE_withConstraints >../Results/ShallowAE/Sparse_NonNeg/KLdivSum_NonNegConstraint/TestOutputs/18_09_08_dim100_goodweights &
+nohup python main.py 2>../Logs/18_09_10_log_100_ShallowAE_SparseNonNeg_withPADO_withOriginals >../Results/ShallowAE/SeveralChannels/WithPADO/Sparse_NonNeg/KLdivSum_NonNegConstraint/TestOutputs/18_09_10_dim100_PADO_originals &
 deactivate
+
