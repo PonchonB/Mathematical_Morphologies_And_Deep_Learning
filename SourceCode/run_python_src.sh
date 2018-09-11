5234bf50ced14b74bf9c771ab03c7f4dbd68383f
PonchonB <bastien.ponchon@gmail.com>
2018-09-11 19:51:03 +0200

    linear SVM

--- SourceCode/run_python_src.sh
+++ SourceCode/run_python_src.sh
@@ -2,6 +2,6 @@
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=1
 source /home/ponchon/StageENV/bin/activate
-nohup python main.py 2>../Logs/18_09_10_log_100_ShallowAE_SparseNonNeg_withPADO_withOriginals >../Results/ShallowAE/SeveralChannels/WithPADO/Sparse_NonNeg/KLdivSum_NonNegConstraint/TestOutputs/18_09_10_dim100_PADO_originals &
+nohup python main.py 2>../Logs/18_09_11_log_100_ShallowAE_Simple_withPADO_withOriginals >../Results/ShallowAE/SeveralChannels/WithPADO/Simple/TestOutputs/18_09_11_dim100_PADO_originals &
 deactivate
 
