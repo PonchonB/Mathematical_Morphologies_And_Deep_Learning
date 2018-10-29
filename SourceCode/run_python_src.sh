911deb51c387156cfa9a6eb13073bd7d38204f86
PonchonB <bastien.ponchon@gmail.com>
2018-10-29 17:27:00 +0100

    Hoyer regularizer test on shallowAE

--- SourceCode/run_python_src.sh
+++ SourceCode/run_python_src.sh
@@ -2,6 +2,6 @@
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=2
 source /home/ponchon/StageENV/bin/activate
-nohup python main.py 2>../Logs/18_10_25_log_100_AsymAE_SparseNonNeg_withAMD_noOriginals >../Results/AsymAE_infoGAN/SeveralChannels/WithAMD_NoOriginals/Sparse_NonNeg/KLdivSum_NonNegConstraint/TestOutputs/18_10_25_dim100_AMD_NoOriginals &
+nohup python main.py 2>../Logs/18_10_29_log_100_ShallowAE_SparseNonNeg_Hoyer >../Results/ShallowAE/Sparse_NonNeg/Hoyer_NonNegConstraint/TestOutputs/18_10_29_dim100_first_Hoyer &
 deactivate
 
