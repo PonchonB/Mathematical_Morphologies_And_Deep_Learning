103308c7a9de96e45ef7dcff647d92727969b7d2
PonchonB <bastien.ponchon@gmail.com>
2018-12-03 15:17:33 +0100

    Testing with dropouts - RESULTS ARE NOT BETTER !

--- SourceCode/run_python_src.sh
+++ SourceCode/run_python_src.sh
@@ -2,6 +2,6 @@
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=3
 source /home/ponchon/StageENV/bin/activate
-nohup python main.py 2>../Logs/18_11_30_log_100_ShallowAE_MaxPlus_NonNeg_500epochs_WithDropout >../Results/ShallowAE_MaxPlus/NonNegativity/Between0and1Constraint/TestOutputs/18_11_30_dim100_500epochs_with_dropout &
+nohup python main.py 2>../Logs/18_11_30_log_100_ShallowAE_MaxPlus_Sparse_NonNeg_500epochs_WithDropout >../Results/ShallowAE_MaxPlus/Sparse_NonNeg/KLdivSum_Between0and1Constraint/TestOutputs/18_11_30_dim100_500epochs_with_dropout &
 deactivate
 
