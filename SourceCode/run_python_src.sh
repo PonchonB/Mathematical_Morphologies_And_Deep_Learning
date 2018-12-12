594449ba8b4133e1e7f0c86d3b8703c444d237d6
PonchonB <bastien.ponchon@gmail.com>
2018-12-12 16:36:00 +0100

    Modif

--- SourceCode/run_python_src.sh
+++ SourceCode/run_python_src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=2
 source /home/ponchon/StageENV/bin/activate
 nohup python main.py 2>../Logs/18_12_11_log_100_AsymAE_MaxPlus_500epochs_WithoutDropout >../Results/AsymAE_MaxPlus/Simple/TestOutputs/18_12_11_dim100_500epochs_without_dropout &
 deactivate
