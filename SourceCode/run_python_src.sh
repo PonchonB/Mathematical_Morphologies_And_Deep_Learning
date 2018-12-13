5e7f4d6ad055528265e5378da0a60b6308a2e737
PonchonB <bastien.ponchon@gmail.com>
2018-12-13 11:50:08 +0100

    BugFix

--- SourceCode/run_python_src.sh
+++ SourceCode/run_python_src.sh
@@ -2,6 +2,6 @@
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=2
 source /home/ponchon/StageENV/bin/activate
-nohup python main.py 2>../Logs/18_12_12_log_100_AsymAE_MaxPlus_NonNeg_500epochs_WithoutDropout >../Results/AsymAE_MaxPlus/NonNegativity/NonNegConstraint/TestOutputs/18_12_12_dim100_500epochs_without_dropout &
+nohup python main.py 2>../Logs/18_12_13_log_100_AsymAE_infoGAN_NonNeg_500epochs >../Results/AsymAE_infoGAN/NonNegativity/NonNegConstraint/TestOutputs/18_12_13_dim100_500epochs &
 deactivate
 
